40d934ddca3a90fc43b050ee0a2e5541744e59d7
Khalid Alanzi <khalidalanzi@Khalids-MacBook-Air.local>
2023-11-14 01:39:08 +0300

    final3

--- reg.js
+++ reg.js
@@ -12,30 +12,34 @@ singupBtn.addEventListener("click", ()=>{
 
     if(Password.length >= 8 && UserN.length >= 5){
 
-                        fetch("https://6552924d5c69a779032a2d8c.mockapi.io/usersInfo", {
-                            method: "POST",
-                            body: JSON.stringify({
-                                UserN,
-                                Email,
-                                Password
-                            })
-                          ,headers: {
-                                'Content-type': 'application/json; charset=UTF-8',
-                            }
-                        })
-                            .then(res => res.json())
-                            .then(data => {console.log(data);})
-                    
-                    
-                        }if(Password.length >= 8 && UserN.length < 5){
-                            alert("The user Name should be 5 letters and above")
-                        }else if(Password.length < 8 && UserN.length >= 5){
-                            alert("The Password should be 8 letters and above")
-                        }else if(Password.length < 8 && UserN.length < 5){
-                            alert("The user Name should be 5 letters and above and password above 8")
-                        }
 
-                    }
+
+
+             fetch("https://6552924d5c69a779032a2d8c.mockapi.io/usersInfo", {
+                   method: "POST",
+                  body: JSON.stringify({
+                    UserN,
+                     Email,
+                   Password
+          })
+            ,headers: {
+               'Content-type': 'application/json; charset=UTF-8',
+           }
+       })
+                   .then(res => res.json())
+                   .then(data => {console.log(data);})
+                   alert("You have Singed Up successfully")
+                   window.location.href = "loggedin.html"  
+                    
+         }else if(Password.length >= 8 && UserN.length < 5){
+                  alert("The user Name should be 5 letters and above")
+         }else if(Password.length < 8 && UserN.length >= 5){
+                   alert("The Password should be 8 letters and above")
+       }else if(Password.length < 8 && UserN.length < 5){
+               alert("The user Name should be 5 letters and above and password above 8")
+            }
+
+     }
 
 )
           
